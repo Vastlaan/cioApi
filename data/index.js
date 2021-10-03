5ce119c08c24c50b3b76a0d73dc6adeeee7ad758
michal.antczak <michal.antczak@and.digital>
2021-10-03 12:17:03 +0200

    Added two test clients

--- data/index.js
+++ data/index.js
@@ -6,6 +6,20 @@ const users = [
     veryficationCode: "12341234",
     fileUrl: "image-1.jpg",
   },
+  {
+    firstName: "John",
+    lastName: "Smith",
+    email: "test@example.com",
+    veryficationCode: "12341234",
+    fileUrl: "image-2.jpg",
+  },
+  {
+    firstName: "Peggy",
+    lastName: "Brown",
+    email: "test@test.com",
+    veryficationCode: "ABCD1234",
+    fileUrl: "image-3.jpg",
+  },
 ];
 
 module.exports = {
